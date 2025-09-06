a5b1beee02c85f2f5f181d121b6a92d8e6508b2d
DevHgL <hleomelo1@icloud.com>
2025-09-06 11:15:28 -0300

    Tudo OK.

--- Tarefa-5/main.cpp
+++ Tarefa-5/main.cpp
@@ -1,40 +1,36 @@
 #include "Image.h"
-#include "rasterization.h"
 
-int main() {
-    int width = 500;
-    int height = 500;
-    ImageRGB image(width, height);
-    image.fill(white);
+float G(float x, float y, float cx, float cy, float s){
+	float dx = x - cx;
+	float dy = y - cy;
+	return 100*exp(-(dx*dx + dy*dy)/(s*s));
+}
 
-    // Define os pontos das linhas a serem desenhadas
-    vec2 p1 = {50, 50};
-    vec2 p2 = {450, 450}; // Linha diagonal principal
+float F(float x, float y){
+	return 50   
+		- G(x, y, 300, 220, 95) //corpo
+        - G(x, y, 300, 340, 60)//corpo
+        - G(x, y, 300, 450, 40)//cabeça
+        - G(x, y, 355, 120, 30)//perna direita
+        - G(x, y, 355, 70, 25)// pé direito
+        - G(x, y, 233, 130, 30)//perna esquerda
+        - G(x, y, 180, 140, 25)//pé esquerdo
+        - G(x, y, 220, 330, 20)// ombro esquerdo
+        - G(x, y, 180, 350, 20)//cotovelo esquerda
+        - G(x, y, 140, 360, 18)//mão esquerda
+        - G(x, y, 380, 330, 20)//ombro direito
+        - G(x, y, 420, 350, 20)//cotovovelo direito
+        - G(x, y, 460, 360, 18);//mão direita
+}
 
-    vec2 p3 = {20, 480};
-    vec2 p4 = {480, 400}; // Linha com inclinação suave
+int main(){
+	ImageRGB img(600, 600);
+	img.fill(white);
+	for(int y = 0; y < img.height(); y++)
+		for(int x = 0; x < img.width(); x++)
+			if(F(x,y) <= 0)
+				img(x,y) = red;
+		
 
-    vec2 p5 = {480, 20};
-    vec2 p6 = {400, 480}; // Linha com inclinação íngreme
-    
-    // Rasteriza e desenha as linhas
-    for (Pixel p : rasterizeLine(std::array<vec2, 2>{p1, p2})) {
-        if (p.x >= 0 && p.x < width && p.y >= 0 && p.y < height)
-            image(p.x, p.y) = red;
-    }
-
-    for (Pixel p : rasterizeLine(std::array<vec2, 2>{p3, p4})) {
-        if (p.x >= 0 && p.x < width && p.y >= 0 && p.y < height)
-            image(p.x, p.y) = green;
-    }
-
-    for (Pixel p : rasterizeLine(std::array<vec2, 2>{p5, p6})) {
-        if (p.x >= 0 && p.x < width && p.y >= 0 && p.y < height)
-            image(p.x, p.y) = blue;
-    }
-
-    image.save("Tarefa-5/linhas_rasterizadas.png");
-    std::cout << "Imagem salva como 'linhas_rasterizadas.png'" << std::endl;
-
-    return 0;
+	img.save("Boneco.png");
 }
---
+++ Tarefa-5/rasterization.h
@@ -0,0 +1,193 @@
+#ifndef RASTERIZATION_H
+#define RASTERIZATION_H
+
+#include <algorithm>
+#include <cmath>
+#include "vec.h"
+
+//////////////////////////////////////////////////////////////////////////////
+
+struct Pixel{
+	int x, y;
+};
+
+inline Pixel toPixel(vec2 u){
+	return { (int)round(u[0]), (int)round(u[1]) };
+}
+
+inline vec2 toVec2(Pixel p){
+	return {(float)p.x, (float)p.y};
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+template<class Line>
+std::vector<Pixel> rasterizeLine(const Line& P){
+	//return simple(P[0], P[1]);
+
+	return dda(P[0], P[1]);
+
+	//return bresenham(toPixel(P[0]), toPixel(P[1]));
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+inline std::vector<Pixel> simple(vec2 A, vec2 B){
+	std::vector<Pixel> out;
+	vec2 d = B - A;
+	float m = d[1]/d[0];
+	float b = A[1] - m*A[0];
+
+	int x0 = (int)roundf(A[0]);
+	int x1 = (int)roundf(B[0]);
+
+	for(int x = x0; x <= x1; x++){
+		int y = (int)roundf(m*x + b);
+		out.push_back({x, y});
+	}
+	return out;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+inline std::vector<Pixel> dda(vec2 A, vec2 B){
+	vec2 dif = B - A;
+	float delta = std::max(fabs(dif[0]), fabs(dif[1]));
+
+	vec2 d = (1/delta)*dif;
+	vec2 p = A;
+
+	std::vector<Pixel> out;
+	for(int i = 0; i <= delta; i++){
+		out.push_back(toPixel(p));
+		p = p + d;
+	}
+	return out;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+inline std::vector<Pixel> bresenham_base(Pixel p0, Pixel p1){
+	std::vector<Pixel> out;
+	int dx = p1.x - p0.x;
+	int dy = p1.y - p0.y;
+	int D = 2*dy - dx; 
+	int y = p0.y;
+	for(int x = p0.x; x <= p1.x; x++){
+		out.push_back({x, y});
+		if(D > 0){
+			y++;
+			D -= 2*dx;
+		}
+		D += 2*dy;
+	}
+	return out;
+}
+
+inline std::vector<Pixel> bresenham(Pixel p0, Pixel p1){
+	if(p0.x > p1.x)
+		std::swap(p0, p1);
+
+	bool mirrorV = p1.y < p0.y;
+	if(mirrorV)
+		p1.y = 2*p0.y - p1.y;
+
+	bool flip = (p1.x - p0.x) < (p1.y - p0.y);
+	if(flip){
+		std::swap(p0.x, p0.y);
+		std::swap(p1.x, p1.y);
+	}
+
+	std::vector<Pixel> out = bresenham_base(p0, p1);
+
+	if(flip){
+		for(Pixel& p: out)
+			std::swap(p.x, p.y);
+		std::swap(p0.x, p0.y);
+	}
+
+	if(mirrorV){
+		for(Pixel& p: out)
+			p.y = 2*p0.y - p.y;
+	}
+	return out;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+template<class Tri>
+std::vector<Pixel> rasterizeTriangle(const Tri& P){
+	return simple_rasterize_triangle(P);
+	//return scanline(P);
+}
+
+template<class Tri>
+std::vector<Pixel> simple_rasterize_triangle(const Tri& P){
+	vec2 A = P[0];
+	vec2 B = P[1];
+	vec2 C = P[2];
+
+	int xmin =  ceil(std::min({A[0], B[0], C[0]}));
+	int xmax = floor(std::max({A[0], B[0], C[0]}));
+	int ymin =  ceil(std::min({A[1], B[1], C[1]}));
+	int ymax = floor(std::max({A[1], B[1], C[1]}));
+
+	std::vector<Pixel> out;
+	Pixel p;
+	for(p.y = ymin; p.y <= ymax; p.y++)
+		for(p.x = xmin; p.x <= xmax; p.x++)
+			if(is_inside(toVec2(p), P))
+				out.push_back(p);
+	return out;
+}
+
+inline float intersection(vec2 A, vec2 B, int y){
+
+	float x = (((B[0] - A[0]) * (y - A[1])) / (B[1] - A[1])) + A[0];
+
+	if ((y < B[1] && y < A[1]) || (y > B[1] && y > A[1]))
+	{
+		return NAN;
+	}
+	else if (y == B[1] && y == A[1])
+	{
+		return A[0];
+	}
+	else
+	{
+		return x;
+	}
+}
+
+template<class Tri>
+std::vector<Pixel> scanline(const Tri& P){
+	vec2 A = P[0];
+	vec2 B = P[1];
+	vec2 C = P[2];
+
+	int ymin =  (int)ceilf(fmin(fmin(A[1], B[1]), C[1]));
+	int ymax = (int)floorf(fmax(fmax(A[1], B[1]), C[1]));
+	float xmax, xmin;
+
+	std::vector<Pixel> out;
+	Pixel p;
+
+	for (p.y = ymin; p.y <= ymax; p.y++)
+	{
+		xmax = fmax(fmax(intersection(A, B, p.y), intersection(B, C, p.y)), intersection(C, A, p.y));
+		xmin = fmin(fmin(intersection(A, B, p.y), intersection(B, C, p.y)), intersection(C, A, p.y));
+
+		int xi = (int)ceilf(xmin);
+		int xf = (int)floorf(xmax);
+		
+		for (p.x = xi; p.x <= xf; p.x++)
+		{
+			out.push_back(p);
+		}
+		
+	}
+	
+	return out;
+}
+
+#endif
